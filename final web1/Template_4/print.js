5e323e3f2fa5c141b66e7c81f64c0088632464ec
otokorkotashvili <132017045+otokorkotashvili@users.noreply.github.com>
2024-01-21 19:08:59 +0400

    Add files via upload

---
+++ final web1/Template_4/print.js
@@ -0,0 +1,32 @@
+
+					let query = location.search;
+					console.log(query);//alert(query);
+					query = query.substring(1,query.length);
+					console.log(query);
+					let querySplit = query.split("&");
+					console.log(querySplit);
+					let Pmpn = querySplit[0].split("=")[1];
+					console.log("Pmpn = " + Pmpn);
+					
+					let Fname = querySplit[1].split("=")[1];
+					console.log("Fname = " + Fname);
+					let Lname = querySplit[2].split("=")[1];
+					console.log("Lname = " + Lname);
+					let TariRi = querySplit[3].split("=")[1];
+					console.log("TariRi = " + TariRi);
+					let Uzmomin = querySplit[4].split("=")[1] * 1;
+					console.log("Uzmomin = " + Uzmomin);
+					let uzmomax = querySplit[5].split("=")[1] * 1;
+					console.log("uzmomax = " + uzmomax);
+					let samxarimin = querySplit[6].split("=")[1] * 1;
+					console.log("samxarimin = " + samxarimin);
+					let samxarimax = querySplit[7].split("=")[1] * 1;
+					console.log("samxarimax = " + samxarimax);
+					let sadilimin = querySplit[8].split("=")[1] * 1;
+					console.log("sadilimin = " + sadilimin);
+					let sadilimax = querySplit[9].split("=")[1] * 1;
+					console.log("sadilimax = " + sadilimax);
+					let vaxSamimin = querySplit[10].split("=")[1] * 1;
+					console.log("vaxSamimin = " + vaxSamimin);
+					let vaxSamimax = querySplit[11].split("=")[1] * 1;
+					console.log("vaxSamimax = " + vaxSamimax);
